54bb875de9aa7ab046f01a6d10825e56304bdcd0
Mediev1l <bartosz5903@gmail.com>
2019-11-24 18:04:20 +0100

    Using debugging tools and threads

--- main.py
+++ main.py
@@ -2,42 +2,52 @@ import time
 import mss
 import pyautogui
 from pynput.keyboard import Key, Listener
+import pynput
 import threading
 
-boundries = ()
+
+run = False
 
 
-def key_thread():
-    with Listener(on_press=on_press) as lis:
-        lis.join()
+boundries = (670, 159, 1200, 1000)
+x_axis = (743, 880, 1030, 1174)
+
+tab = [0] * 4
 
 
 def on_press(key):
-    return key
+    global run
 
+    try: k = key.char # single-char keys
+    except: k = key.name # other keys
+    print(k)
+    if key == Key.esc:
+        run = False
+    if k == 's':
+        tab[1] = 10
+        run = True
 
-
-def exit_(ASCII_key):
-    hit = msvcrt.kbhit()
-
-    if hit:
-        return msvcrt.getch()
-    else:
-        return None
-
-
-if __name__ == '__main__':
-
-    key_thread = threading.Thread(target=key_thread)
-    key_thread.start()
-
-
+def on_click(x, y, button, pressed):
     with mss.mss() as sct:
         monitor = sct.monitors[1]
 
         shot = sct.grab(monitor)
+        print(x,y)
+        print(shot.pixel(x, y))
 
-        time.sleep(2)
+
+
+
+if __name__ == '__main__':
+
+    key_thread = Listener(on_press = on_press)
+    key_thread.start()
+
+    mouse_thread = pynput.mouse.Listener(on_click=on_click)
+    mouse_thread.start()
+
+
+
 
 
 
@@ -49,8 +59,14 @@ if __name__ == '__main__':
     #     listener.start()
 
 
-    while key_thread.:
-        print(threading.active_count())
-        key_thread= threading.Thread(target=key_thread)
-
-        print(pyautogui.position())
+    while True:
+        pass
+        # with mss.mss() as sct:
+        #     monitor = sct.monitors[1]
+        #
+        #     shot = sct.grab(monitor)
+        #
+        #
+        #     time.sleep(1)
+        #     print(pyautogui.position())
+        #     print(shot.pixel(pyautogui.position().x, pyautogui.position().y))
